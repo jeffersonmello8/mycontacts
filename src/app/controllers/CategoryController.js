0ebff9357715fbce892ff0b7db5f5c386be9230e
Jefferson Sousa <jefferson.mello8@hotmail.com>
2023-06-01 09:30:20 -0300

    adding categories resources (get, post, delete)

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,35 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (await CategoriesRepository.findByName({ name })) {
+      return response.status(400).json({ error: 'Name already in use' });
+    }
+
+    const newCategory = await CategoriesRepository.create({ name });
+
+    response.json(newCategory);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new CategoryController();
---
+++ src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,40 @@
+const db = require('../../database');
+
+class CategoriesRepository {
+  async findAll() {
+    const rows = await db.query('SELECT * FROM categories ORDER BY name');
+    return rows;
+  }
+
+  async findByName(name) {
+    const [row] = await db.query(
+      'SELECT * from categories WHERE name = $1',
+      [name],
+    );
+
+    return row;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(
+      `
+      INSERT INTO categories(name)
+      VALUES ($1)
+      RETURNING *`,
+      [name],
+    );
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOp = await db.query(
+      'DELETE FROM categories WHERE id = $1',
+      [id],
+    );
+
+    return deleteOp;
+  }
+}
+
+module.exports = new CategoriesRepository();
